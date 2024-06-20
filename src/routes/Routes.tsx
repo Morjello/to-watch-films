22f30c260764eedeed2e98c3ff8d284370b6e1db
morjello <morjello@yandex.ru>
2024-06-20 21:17:10 +0300

    second test

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import SearchPage from '../components/SearchPage/SearchPage';
 import SingleMoviePage from '../components/SingleMoviePage/SingleMoviePage';
+import SeasonsList from '../components/Test/Test';
 
 const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/test" element={<SeasonsList />} />
 			<Route path="/movies" element={<SearchPage />} />
 			<Route path={`/movies/:movieId`} element={<SingleMoviePage />} />
 		</Routes>
